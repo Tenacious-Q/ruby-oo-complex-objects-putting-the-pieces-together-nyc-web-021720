6feb96ff19a57b3ea61030728b7dd5876fa97862
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-24 18:49:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
 
   attr_reader :brand
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   attr_writer :material
 
   def initialize(brand)
